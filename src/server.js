5f91bf1ce4d50f0033e231f4ef4da529c5efe095
Juan Plasencia <jplasencia.dev@outlook.com>
2024-07-11 20:51:04 -0500

    simple set up API

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Welcome to Med Connect API"));
+
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || "localhost";
+
+app.listen(port, host, () => {
+  console.log(`Server listening on ${host}:${port}`);
+});
