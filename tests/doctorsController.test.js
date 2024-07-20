b3f1c5fa19e8a9cf6a91d3216248746f0fa47f22
baileynance <baieyj.nance@gmail.com>
2024-07-19 20:26:39 -0700

    added test function for getting doctor by doctor id

--- tests/doctorsController.test.js
+++ tests/doctorsController.test.js
@@ -1,4 +1,47 @@
-// Fake Test to clean up process as we build these
-test('dummy test', () => {
-    expect(true).toBe(true);
+// Doctor Record Test Function
+const { getDoctorById } = require('../src/controllers/doctorsController');
+const doctor = require('../src/models/Doctor');
+const mongoose = require('mongoose');
+
+// Mock the Doctor model
+jest.mock('../src/models/Doctor');
+
+// Describe Doctor
+describe('Doctors Controller', () => {
+  afterAll(() => {
+    mongoose.connection.close();
   });
+
+  describe('getDoctorById', () => {
+    it('should return doctor by doctor ID', async () => {
+      const mockDoctorRecords = [{firstName: 'Will'}, {lastName: 'Williams'}];
+      doctor.find.mockResolvedValue(mockDoctorRecords);
+
+      const req = { params: { doctorId: '777' } };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+      };
+
+      await getDoctorById(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith(mockDoctorRecords);
+    });
+
+    it('should return 404 if no doctor records found', async () => {
+      doctor.find.mockResolvedValue([]);
+
+      const req = { params: { doctorId: '777' } };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+      };
+
+      await getDoctorById(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith({ error: 'No doctor records found' });
+    });
+  });
+});
