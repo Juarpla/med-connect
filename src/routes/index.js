4eb86f5bfa767aa7dfb56b3a5a4c901ee809e06c
Juan Plasencia <jplasencia.dev@outlook.com>
2024-07-20 02:32:29 -0500

    set configuration to use OAuth logout user session

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const homeController = require("../controllers/homeController");
 
 router.get("/", homeController.root);
+router.use("/auth", require("./auth"));
 router.use("/patients", require("./patients"));
 router.use("/doctors", require("./doctors"));
 router.use("/appointments", require("./appointments"));
 router.use("/medical-records", require("./medicalRecords"));
 router.use("/prescriptions", require("./prescriptions"));
-router.use("/auth", require("./auth"));
 
 module.exports = router;
