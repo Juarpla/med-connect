fc8eb350b6483d5b32177f2705cb21827781ea8d
Juan Plasencia <jplasencia.dev@outlook.com>
2024-07-20 17:41:12 -0500

    Set protected routes by OAuth2 - Part 1

--- src/routes/appointments.js
+++ src/routes/appointments.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const isAuthenticated = require("../utils/authHelpers").isAuthenticated;
 const {
   getAllAppointments,
   getAppointmentById,
@@ -14,8 +15,20 @@ router.get("/", getAllAppointments);
 router.get("/:appointmentId", getAppointmentById);
 router.get("/doctor/:doctorId", getAppointmentsByDoctorId);
 router.get("/patient/:patientId", getAppointmentsByPatientId);
-router.post("/", createAppointment);
-router.put("/:appointmentId", updateAppointment);
-router.delete("/:appointmentId", deleteAppointment);
+router.post(
+  "/",
+  isAuthenticated,
+  createAppointment
+);
+router.put(
+  "/:appointmentId",
+  isAuthenticated,
+  updateAppointment
+);
+router.delete(
+  "/:appointmentId",
+  isAuthenticated,
+  deleteAppointment
+);
 
 module.exports = router;
--- src/utils/authHelpers.js
+++ src/utils/authHelpers.js
@@ -34,4 +34,11 @@ authHelper.failureObject = {
   session: false,
 };
 
+authHelper.isAuthenticated = (req, res, next) => {
+  if (req.session.user === undefined) {
+    return res.status(401).json("You do not have access.");
+  }
+  next();
+};
+
 module.exports = authHelper;
