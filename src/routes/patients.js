ea98d8be8e22c08cdfc3862b227ae6c814c56763
Juan Plasencia <jplasencia.dev@outlook.com>
2024-07-20 19:38:15 -0500

    Set validation: Patients CRUD

--- src/routes/patients.js
+++ src/routes/patients.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const patientsController = require("../controllers/patientsController");
+const addPatientRules = require("../validations/patientValidation")();
+const addMongoIdRules = require("../validations/mongoIdValidation")();
+const checkData = require("../validations/checkDataHelper");
 
 router.get("/", patientsController.getAllPatients);
-router.get("/:patientId", patientsController.getPatientById);
-router.post("/", patientsController.createPatient);
-router.put("/:patientId", patientsController.updatePatientById);
-router.delete("/:patientId", patientsController.deletePatientById);
+router.get(
+  "/:id",
+  addMongoIdRules,
+  checkData,
+  patientsController.getPatientById,
+);
+router.post("/", addPatientRules, checkData, patientsController.createPatient);
+router.put(
+  "/:id",
+  addMongoIdRules,
+  addPatientRules,
+  checkData,
+  patientsController.updatePatientById,
+);
+router.delete(
+  "/:id",
+  addMongoIdRules,
+  checkData,
+  patientsController.deletePatientById,
+);
 
 module.exports = router;
---
+++ src/validations/checkDataHelper.js
@@ -0,0 +1,12 @@
+const { validationResult } = require("express-validator");
+
+const checkData = async (req, res, next) => {
+  let errors = [];
+  errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+module.exports = checkData;
